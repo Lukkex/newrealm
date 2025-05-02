6828ab38688e98fc6ec97232499d455933e64a90
Lukkex <87504872+Lukkex@users.noreply.github.com>
2025-05-02 02:15:38 -0700

    Removed a TON of clutter; WIP Animations and Mouselook, First Person view to be added next

--- Client/newrealm/MyGame.java
+++ Client/newrealm/MyGame.java
@@ -36,7 +36,7 @@ public class MyGame extends VariableFrameRateGame
 
 	private int fluffyClouds; //skyboxes
 
-	private boolean hasWon, gameOver, sphereDisarmed, cubeDisarmed, torusDisarmed, isDisarming, callOnce = false;
+	private boolean hasWon, gameOver, callOnce = false;
 	private boolean worldAxisOn = true;
 
 	private double lastFrameTime, currFrameTime, elapsTime;
@@ -45,9 +45,6 @@ public class MyGame extends VariableFrameRateGame
 	private final float DEFAULT_SPEED = 0.02f;
 	private float movementSpeed = DEFAULT_SPEED;
 	private float lineLength = 5.0f;
-	private float disarmDistance = 3.5f;
-	private float warningDistance = 6.0f;
-	private float detonationDistance = 4.0f;
 	private float height = 0.0f;
 
 	private String broadcastMessage = "";
@@ -63,25 +60,22 @@ public class MyGame extends VariableFrameRateGame
 	private Vector3f hudColor = hudWhiteColor;
 	private Vector3f loc;
 
-	private GameObject avatar, sphere_sat, 
-	cube_sat, torus_sat, lineX, lineY, lineZ, 
-	enemy, floor, ground, spaceText, wireR, wireG, wireB,
-	chamber;
+	private GameObject avatar, lineX, lineY, lineZ, 
+	ghoul, floor, ground, chamber;
 
-	private ObjShape avatarS, ghostS, sphereS, cubeS, torusS, lineSx, 
-	lineSy, lineSz, enemyS, floorS, groundS, spaceTextS, wireS, chamberS;
+	private ObjShape avatarS, ghostS, lineSx, 
+	lineSy, lineSz, floorS, groundS, chamberS;
 
-	private TextureImage avatartx, ghosttx, spheretx, s_safetx, s_disarmtx, 
-	cubetx, c_safetx, c_disarmtx, torustx, t_safetx, t_disarmtx, 
-	linetx, enemytx, floortx, groundtx, floorheightmap, groundheightmap, 
-	detonatedtx, spaceTexttx, wireRtx, wireGtx, wireBtx, chambertx;
+	private AnimatedShape ghoulS;
+
+	private TextureImage avatartx, ghosttx, linetx, ghoultx, 
+	floortx, groundtx, floorheightmap, groundheightmap, chambertx;
 
 	private Light light1, light2, light3, enemyLight;
 
 	private RotationController rc;
 	private BobbingController bc;
 	private CameraOrbit3D camOrbitController;
-	private CameraMinimap minimapController;
 
 	private String serverAddress;
 	private int serverPort;
@@ -149,20 +143,12 @@ public class MyGame extends VariableFrameRateGame
 	{	
 		avatarS = new ImportedModel("GHOULhead.obj");
 		ghostS = new ImportedModel("GHOULhead.obj");
-		sphereS = new Sphere();
-		cubeS = new Cube();
-		torusS = new Torus();
-		enemyS = new ImportedModel("GHOUL.obj");
+		ghoulS = new AnimatedShape("Ghoul.rkm", "Ghoul.rks");
+		ghoulS.loadAnimation("IDLE", "GhoulIdle.rka");
 		floorS = new TerrainPlane(1000);
 		groundS = new TerrainPlane(100);
 
-		//Custom space text for after disarms
-		spaceTextS = new ImportedModel("spacetext.obj");
-
 		chamberS = new ImportedModel("Chamber.obj");
-
-		//Satellite wire shape
-		wireS = new ImportedModel("wire.obj");
 
 		//X, Y, & Z Axes Lines
 		lineSy = new Line(new Vector3f(0.0f, 0.0f, 0.0f), new Vector3f(0.0f, lineLength, 0.0f));
@@ -176,37 +162,17 @@ public class MyGame extends VariableFrameRateGame
 		avatartx = new TextureImage("GHOUL.jpg");
 		ghosttx = new TextureImage("GHOUL.jpg");
 
-		spheretx = new TextureImage("Sphere_Satellite.png");
-		s_safetx = new TextureImage("Sphere_Close.png");
-		s_disarmtx = new TextureImage("Sphere_Disarmed.png");
-
-		cubetx = new TextureImage("Cube_Satellite.png");
-		c_safetx = new TextureImage("Cube_Close.png");
-		c_disarmtx = new TextureImage("Cube_Disarmed.png");
-
-		torustx = new TextureImage("Torus_Satellite.png");
-		t_safetx = new TextureImage("Torus_Close.png");
-		t_disarmtx = new TextureImage("Torus_Disarmed.png");
-
 		floortx = new TextureImage("ground.jpg");
 		floorheightmap = new TextureImage("floorheightmap.jpg");
 
 		groundtx = new TextureImage("wireG.png");
 		groundheightmap = new TextureImage("sanfrancisco.jpg");
 
-		detonatedtx = new TextureImage("detonated.png");
-
 		linetx = new TextureImage("line.png");
 
-		enemytx = new TextureImage("GHOUL.jpg");
-
-		spaceTexttx = new TextureImage("whiteText.png");
+		ghoultx = new TextureImage("GHOUL.jpg");
 
 		chambertx = new TextureImage("Chamber.jpg");
-
-		wireRtx = new TextureImage("wireR.png");
-		wireGtx = new TextureImage("wireG.png");
-		wireBtx = new TextureImage("wireB.png");
 	}
 
 	@Override
@@ -221,10 +187,6 @@ public class MyGame extends VariableFrameRateGame
 		// build avatar in the center of the window
 		avatar = new GameObject(GameObject.root(), avatarS, avatartx);
 		
-		sphere_sat = new GameObject(GameObject.root(), sphereS, spheretx);
-		cube_sat = new GameObject(GameObject.root(), cubeS, cubetx);
-		torus_sat = new GameObject(GameObject.root(), torusS, torustx);
-
 		lineX = new GameObject(GameObject.root(), lineSx, linetx);
 		lineY = new GameObject(GameObject.root(), lineSy, linetx);
 		lineZ = new GameObject(GameObject.root(), lineSz, linetx);
@@ -236,47 +198,14 @@ public class MyGame extends VariableFrameRateGame
 
 		ground = new GameObject(GameObject.root(), groundS, groundtx);
 
-		enemy = new GameObject(GameObject.root(), enemyS, enemytx);
-
-		spaceText = new GameObject(GameObject.root(), spaceTextS, spaceTexttx);
+		ghoul = new GameObject(GameObject.root(), ghoulS, ghoultx);
 
 		chamber = new GameObject(GameObject.root(), chamberS, chambertx);
 
-		//Satellite wires
-		wireR = new GameObject(torus_sat, wireS, wireRtx);
-		wireG = new GameObject(sphere_sat, wireS, wireGtx);
-		wireB = new GameObject(cube_sat, wireS, wireBtx);
-
-		wireR.setLocalTranslation((new Matrix4f()).translation(0f,1f,-0.2f));
-		wireG.setLocalTranslation((new Matrix4f()).translation(0f,1f,0f));
-		wireB.setLocalTranslation((new Matrix4f()).translation(0f,1f,0.2f));
-
-		wireR.setLocalScale((new Matrix4f()).scaling(0.1f));
-		wireG.setLocalScale((new Matrix4f()).scaling(0.1f));
-		wireB.setLocalScale((new Matrix4f()).scaling(0.1f));
-
-		wireR.applyParentRotationToPosition(false);
-		wireG.applyParentRotationToPosition(false);
-		wireB.applyParentRotationToPosition(false);
-
-		wireR.propagateRotation(false);
-		wireG.propagateRotation(false);
-		wireB.propagateRotation(false);
-
 		avatar.setLocalTranslation((new Matrix4f()).translation(0f,0f,0f));
-		//avatar.setLocalScale((new Matrix4f()).scaling(3.0f));
 
-		sphere_sat.setLocalTranslation((new Matrix4f()).translation(15.0f, 0.0f, 15.0f));
-		sphere_sat.setLocalScale((new Matrix4f()).scaling(1.0f));
-
-		cube_sat.setLocalTranslation((new Matrix4f()).translation(-15.0f, 0.0f, 15.0f));
-		cube_sat.setLocalScale((new Matrix4f()).scaling(1.1f));
-
-		torus_sat.setLocalTranslation((new Matrix4f()).translation(-15.0f, 0.0f, -15.0f));
-		torus_sat.setLocalScale((new Matrix4f()).scaling(0.9f));
-
-		enemy.setLocalTranslation((new Matrix4f()).translation(15.0f, 0.0f, -15.0f));
-		enemy.setLocalScale((new Matrix4f()).scaling(50f));
+		ghoul.setLocalTranslation((new Matrix4f()).translation(15.0f, 0.0f, -15.0f));
+		ghoul.setLocalScale((new Matrix4f()).scaling(1f));
 
 		floor.setLocalTranslation((new Matrix4f()).translation(0f, -1f, 0f));
 		floor.setLocalScale((new Matrix4f()).scaling(25f));
@@ -289,9 +218,6 @@ public class MyGame extends VariableFrameRateGame
 		ground.setHeightMap(groundheightmap);
 		ground.getRenderStates().setTiling(1);
 		ground.getRenderStates().setTileFactor(10);
-
-		spaceText.setLocalTranslation((new Matrix4f()).translation(0f, 2f, 0f));
-		spaceText.setLocalScale((new Matrix4f()).scaling(0.2f));
 
 		chamber.setLocalTranslation((new Matrix4f()).translation(0f, 15f, 0f));
 		chamber.setLocalScale((new Matrix4f()).scaling(8f));
@@ -322,7 +248,7 @@ public class MyGame extends VariableFrameRateGame
 
 		//Light that is inside of the enemy enemy object
 		enemyLight = new Light();
-		enemyLight.setLocation(enemy.getWorldLocation());
+		enemyLight.setLocation(ghoul.getWorldLocation());
 		enemyLight.setDiffuse(1.0f, 0.0f, 0.0f);
 		enemyLight.setSpecular(1.0f, 0.0f, 0.0f);
 
@@ -344,7 +270,6 @@ public class MyGame extends VariableFrameRateGame
 		TurnAction turnRight = new TurnAction(this, protClient, -yawAmount);
 		PitchAction pitchUp = new PitchAction(this, protClient, pitchAmount);
 		PitchAction pitchDown = new PitchAction(this, protClient, -pitchAmount);
-		DisarmAction disarm = new DisarmAction(this);
 		DisableAxisAction disableAxis = new DisableAxisAction(this);
 
 		//Controller actions
@@ -358,11 +283,8 @@ public class MyGame extends VariableFrameRateGame
 		im.associateActionWithAllKeyboards(net.java.games.input.Component.Identifier.Key.D, turnRight, InputManager.INPUT_ACTION_TYPE.REPEAT_WHILE_DOWN);
 		im.associateActionWithAllKeyboards(net.java.games.input.Component.Identifier.Key.UP, pitchUp, InputManager.INPUT_ACTION_TYPE.REPEAT_WHILE_DOWN);
 		im.associateActionWithAllKeyboards(net.java.games.input.Component.Identifier.Key.DOWN, pitchDown, InputManager.INPUT_ACTION_TYPE.REPEAT_WHILE_DOWN);
-		im.associateActionWithAllKeyboards(net.java.games.input.Component.Identifier.Key.E, disarm, InputManager.INPUT_ACTION_TYPE.REPEAT_WHILE_DOWN);
 		im.associateActionWithAllKeyboards(net.java.games.input.Component.Identifier.Key.Q, disableAxis, InputManager.INPUT_ACTION_TYPE.ON_PRESS_ONLY);
 		
-		spaceText.getRenderStates().disableRendering();
-
 		lastFrameTime = System.currentTimeMillis();
 		currFrameTime = System.currentTimeMillis();
 		elapsTime = 0.0;
@@ -372,11 +294,9 @@ public class MyGame extends VariableFrameRateGame
 		// ------------- Setting up main camera and minimap camera -------------
 		
 		cam = (engine.getRenderSystem().getViewport("LEFT").getCamera());
-		minimap = (engine.getRenderSystem().getViewport("RIGHT").getCamera());
 
 		String gamepadName = im.getFirstGamepadName();
 		camOrbitController = new CameraOrbit3D(cam, avatar, gamepadName, engine);
-		minimapController = new CameraMinimap(minimap, floor, gamepadName, engine);
 
 		rc = new RotationController(engine, new Vector3f(0, 1, 0), 0.002f);
 		bc = new BobbingController(engine, 0.0005f);
@@ -390,6 +310,8 @@ public class MyGame extends VariableFrameRateGame
 		rc.addTarget(chamber);
 
 		initMouseMode();
+
+		ghoulS.playAnimation("IDLE", 0.1f, AnimatedShape.EndType.LOOP, 0);
 	}
 
 	@Override
@@ -403,12 +325,11 @@ public class MyGame extends VariableFrameRateGame
 			//Movement speed is based on elapsed time; InputManager gets updated and Action classes use this speed variable
 			movementSpeed = DEFAULT_SPEED * (float) elapsTime;
 
-			isDisarming = false;
 			im.update((float) elapsTime);
+			ghoulS.updateAnimation();
 			processNetworking((float) elapsTime);
 
 			camOrbitController.updateCameraPosition();
-			minimapController.updateCameraPosition();
 
 			//avatarpin cannot go beneath the Y level of the floor plane
 			if (avatar.getWorldLocation().y() < floor.getWorldLocation().y())
@@ -420,9 +341,9 @@ public class MyGame extends VariableFrameRateGame
 			avatar.setLocalLocation(new Vector3f(loc.x(), height, loc.z()));
 
 			//Enemy ManualCube continues to look towards and follow the camera
-			enemy.lookAt(avatar.getWorldLocation());
-			enemy.move(0.01f);
-			enemyLight.setLocation(enemy.getWorldLocation());
+			ghoul.lookAt(avatar.getWorldLocation());
+			ghoul.move(0.01f);
+			enemyLight.setLocation(ghoul.getWorldLocation());
 			
 			//Enemy light is always inside of enemy cube, and will flicker based on the elapsed time
 			if (Math.sin(currFrameTime/100) <= 0.5)
@@ -430,142 +351,8 @@ public class MyGame extends VariableFrameRateGame
 			else
 				enemyLight.disable();
 			
-			//Handles sphere's textures/states
-			if (!sphereDisarmed){
-				if (distance(sphere_sat.getWorldLocation(), avatar.getWorldLocation()) <= detonationDistance){ //Sphere detonates
-					sphere_sat.setTextureImage(detonatedtx);
-					gameOver = true;
-					broadcastMessage = " | SATELLITE DETONATED!";
-					hudColor = hudRedColor;
-				}
-				else if (distance(sphere_sat.getWorldLocation(), avatar.getWorldLocation()) <= warningDistance){ //Sphere is in warning distance
-					sphere_sat.setTextureImage(s_safetx);
-					broadcastMessage = " | WARNING DISTANCE!";
-					hudColor = hudYellowColor;
-
-					if (!gameOver && isDisarming){ //Sphere is disarmed
-						sphere_sat.setTextureImage(s_disarmtx);
-						sphereDisarmed = true;
-						numOfDisarmed++;
-						rc.addTarget(sphere_sat);
-						bc.addTarget(sphere_sat);
-						broadcastMessage = " | SATELLITE DISARMED!";
-						hudColor = hudGreenColor;
-
-						spaceText.propagateRotation(false);
-						spaceText.applyParentRotationToPosition(false);
-						spaceText.setParent(sphere_sat);
-						spaceText.getRenderStates().enableRendering();
-
-						wireG.setParent(avatar);
-					}
-				}	
-			}
-			else {
-				sphere_sat.setTextureImage(s_disarmtx);
-			}
-
-			//Handles cube's textures/states
-			if (!cubeDisarmed){
-				if (distance(cube_sat.getWorldLocation(), avatar.getWorldLocation()) <= detonationDistance){ //Cube detonates
-					cube_sat.setTextureImage(detonatedtx);
-					gameOver = true;
-					broadcastMessage = " | SATELLITE DETONATED!";
-					hudColor = hudRedColor;
-				}
-				else if (distance(cube_sat.getWorldLocation(), avatar.getWorldLocation()) <= warningDistance){ //Cube is in warning distance
-					cube_sat.setTextureImage(c_safetx);
-					broadcastMessage = " | WARNING DISTANCE!";
-					hudColor = hudYellowColor;
-
-					if (!gameOver && isDisarming){ //Cube is disarmed
-						cube_sat.setTextureImage(c_disarmtx);
-						cubeDisarmed = true;
-						numOfDisarmed++;
-						rc.addTarget(cube_sat);
-						bc.addTarget(cube_sat);
-						broadcastMessage = " | SATELLITE DISARMED!";
-						hudColor = hudGreenColor;
-						
-						spaceText.propagateRotation(false);
-						spaceText.applyParentRotationToPosition(false);
-						spaceText.setParent(cube_sat);
-						spaceText.getRenderStates().enableRendering();
-
-						wireB.setParent(avatar);
-					}
-				}
-			}
-			else {
-				cube_sat.setTextureImage(c_disarmtx);
-			}
-
-			//Handles torus's textures/states
-			if (!torusDisarmed){
-				if (distance(torus_sat.getWorldLocation(), avatar.getWorldLocation()) <= detonationDistance){ //Torus detonates
-					torus_sat.setTextureImage(detonatedtx);
-					gameOver = true;
-					broadcastMessage = " | SATELLITE DETONATED!";
-					hudColor = hudRedColor;
-				}
-				else if (distance(torus_sat.getWorldLocation(), avatar.getWorldLocation()) <= warningDistance){ //Torus is in warning distance
-					torus_sat.setTextureImage(t_safetx);
-					broadcastMessage = " | WARNING DISTANCE!";
-					hudColor = hudYellowColor;
-
-					if (!gameOver && isDisarming){ //Torus is disarmed
-						torus_sat.setTextureImage(t_disarmtx);
-						torusDisarmed = true;
-						numOfDisarmed++;
-						rc.addTarget(torus_sat);
-						bc.addTarget(torus_sat);
-						broadcastMessage = " | SATELLITE DISARMED!";
-						hudColor = hudGreenColor;
-
-						spaceText.propagateRotation(false);
-						spaceText.applyParentRotationToPosition(false);
-						spaceText.setParent(torus_sat);
-						spaceText.getRenderStates().enableRendering();
-
-						wireR.setParent(avatar);
-					}
-				}
-			}
-			else {
-				torus_sat.setTextureImage(t_disarmtx);
-			}
-			
-			if (distance(enemy.getWorldLocation(), avatar.getWorldLocation()) <= 4.0f){
-				broadcastMessage = " | ENEMY IS CLOSING IN!";
-				hudColor = hudRedColor;
-			}
-			else if (distance(sphere_sat.getWorldLocation(), avatar.getWorldLocation()) > warningDistance &&
-			distance(cube_sat.getWorldLocation(), avatar.getWorldLocation()) > warningDistance && 
-			distance(torus_sat.getWorldLocation(), avatar.getWorldLocation()) > warningDistance){
-				broadcastMessage = "";
-				hudColor = hudWhiteColor;
-			}
-
-			if (numOfDisarmed == 3){
-				hasWon = true; //Game is won - all satellites were disarmed
-				callOnce = true;
-			}
-			else if (distance(enemy.getWorldLocation(), avatar.getWorldLocation()) <= 1.0f)
-				gameOver = true; //Game is over - the enemy caught you
-		
 			// Broadcast any new message and update HUD 1
 			broadcast(broadcastMessage, hudColor);
-
-			// Update HUD 2 for the smaller viewport
-			vp2X = (int) ((engine.getRenderSystem()).getViewport("RIGHT").getRelativeLeft() * (engine.getRenderSystem()).getWidth());
-			vp2Y = (int) ((engine.getRenderSystem()).getViewport("RIGHT").getRelativeBottom() * (engine.getRenderSystem()).getHeight());
-			(engine.getHUDmanager()).setHUD2("Location: X: " + Float.toString(avatar.getWorldLocation().x()) + ", Y: " + Float.toString(avatar.getWorldLocation().y()), hudWhiteColor, vp2X + 2, vp2Y + 2);
-			
-			spaceText.lookAt(cam.getLocation()); //Text will always face towards the camera
-
-			//Disable space text if it gets too high up in the air
-			if (spaceText.getWorldLocation().y() > 40f)
-				spaceText.getRenderStates().disableRendering();
 		}
 
 		//Calls once more so that all child nodes properly snap to new parents (e.g. wires)
@@ -573,7 +360,6 @@ public class MyGame extends VariableFrameRateGame
 			im.update((float) elapsTime);
 			callOnce = false;
 		}
-		spaceText.lookAt(cam.getLocation());
 		
 		//Player has lost the game
 		if (gameOver){
@@ -589,22 +375,12 @@ public class MyGame extends VariableFrameRateGame
 	@Override
 	public void createViewports(){
 		(engine.getRenderSystem()).addViewport("LEFT",0,0,1f,1f);
-		(engine.getRenderSystem()).addViewport("RIGHT",.75f,0,.25f,.25f);
 		Viewport leftVp = (engine.getRenderSystem()).getViewport("LEFT");
-		Viewport rightVp = (engine.getRenderSystem()).getViewport("RIGHT");
 		Camera leftCamera = leftVp.getCamera();
-		Camera rightCamera = rightVp.getCamera();
-		rightVp.setHasBorder(true);
-		rightVp.setBorderWidth(1);
-		rightVp.setBorderColor(1.0f, 1.0f, 1.0f);
 		leftCamera.setLocation(new Vector3f(-2,0,2));
 		leftCamera.setU(new Vector3f(1,0,0));
 		leftCamera.setV(new Vector3f(0,1,0));
 		leftCamera.setN(new Vector3f(0,0,-1));
-		rightCamera.setLocation(new Vector3f(0,2,0));
-		rightCamera.setU(new Vector3f(1,0,0));
-		rightCamera.setV(new Vector3f(0,0,-1));
-		rightCamera.setN(new Vector3f(0,-1,0));
 	}
 
 	//Broadcasting function that can announce a broadcast message on the HUD
@@ -632,11 +408,6 @@ public class MyGame extends VariableFrameRateGame
 		return avatar;
 	}
 
-	//Sets the disarming state to true or false
-	//boolean is used to determine if player will disarm satellite when in warning proximity
-	public void setDisarming(boolean disarmState){
-		isDisarming = disarmState;
-	}
 
 	//Used by DisableAxisAction.java to toggle the central world axis line objects
 	public void toggleXYZAxis(){
